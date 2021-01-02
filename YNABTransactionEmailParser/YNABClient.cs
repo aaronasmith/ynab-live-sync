69c2a96400a589f5f275e35f603ae0d71ffb3630
Aaron Smith <aaronasmith@gmail.com>
2021-01-02 09:23:55 -0600

    Log when account found.

--- YNABTransactionEmailParser/YNABClient.cs
+++ YNABTransactionEmailParser/YNABClient.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using YNABTransactionEmailParser.Domain.YNAB;
 
 namespace YNABTransactionEmailParser
@@ -9,14 +10,18 @@ namespace YNABTransactionEmailParser
     public class YNABClient
     {
         private readonly HttpClient _httpClient;
-        public YNABClient(HttpClient httpClient)
+        private readonly ILogger<YNABClient> _logger;
+        public YNABClient(ILogger<YNABClient> logger, HttpClient httpClient)
         {
+            _logger = logger;
             _httpClient = httpClient;
         }
 
-        private async Task<string> GetAccountId(string last4Digits){
+        private async Task<string> GetAccountId(string last4Digits)
+        {
             var response = await _httpClient.GetAsync("accounts");
-            if(response.StatusCode == System.Net.HttpStatusCode.NotFound){
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
                 var error = await response.Content.ReadAsAsync<GetAccountResponseError>();
                 throw new System.Exception($"Error retrieving accounts: {error.Error.Detail}");
             }
@@ -28,10 +33,13 @@ namespace YNABTransactionEmailParser
         internal async Task PostTransaction(Transaction transaction)
         {
             var accountId = await GetAccountId(transaction.Last4);
-            if(accountId == null){
+            if (accountId == null)
+            {
                 throw new Exception($"No account found for {transaction.Last4}");
             }
-            await _httpClient.PostAsJsonAsync("transactions", new AddTransactionRequest{
+            _logger.LogInformation("Found account: {id}", accountId);
+            await _httpClient.PostAsJsonAsync("transactions", new AddTransactionRequest
+            {
                 transaction = {
                     account_id = accountId,
                     date = transaction.Date.ToString("yyyy-MM-dd"),
