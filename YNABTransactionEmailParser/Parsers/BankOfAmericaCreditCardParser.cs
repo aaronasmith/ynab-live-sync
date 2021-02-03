02fc6857795351c9fb5a66fc1cd297496632762d
lancewilliams <lancewilliams@eftsource.com>
2021-02-03 16:32:40 -0600

    Completed BoA parser with absolutely no one else's help, especially not A-rone's

--- YNABTransactionEmailParser/Parsers/BankOfAmericaCreditCardParser.cs
+++ YNABTransactionEmailParser/Parsers/BankOfAmericaCreditCardParser.cs
@@ -7,6 +7,7 @@ namespace YNABTransactionEmailParser.Parsers
 
     public class BankOfAmericaCreditCardParser : IParser
     {
+        private static Regex regex = new Regex(@"Credit Card ending in (?<LastFour>\d+).*Amount:\s+\$(?<Amount>[\d\.]+).*Date:\s+(?<Date>\w+\s+\d+,\s+\d+).*Where:\s+(?<Payee>.*(?=\s+View))", RegexOptions.Singleline);
         public Transaction ParseEmail(string contents)
         {
             //check if it's an authorization email
@@ -18,8 +19,17 @@ namespace YNABTransactionEmailParser.Parsers
             }
 
             //try to get transaction information
+            var match = regex.Match(contents);
+            if(!match.Success) return null;
 
-            return null;
+            //January 31, 2021
+            return new Transaction
+            {
+                Amount = decimal.Parse(match.Groups["Amount"]?.Value),
+                Date = DateTime.ParseExact(match.Groups["Date"].Value, "MMMM dd, yyyy", CultureInfo.CurrentCulture),
+                Last4 = match.Groups["LastFour"].Value,
+                Payee = match.Groups["Payee"].Value
+            };
         }
     }
 }
--- YNABTransactionEmailParserTests/BankOfAmericaCreditCardTests.cs
+++ YNABTransactionEmailParserTests/BankOfAmericaCreditCardTests.cs
@@ -20,6 +20,16 @@ public class BankOfAmericaAuthorizationTests{
     }
 
     [Fact]
+    public void ParsesTransactionEmailCorrectly(){
+        var transaction = parser.ParseEmail(File.ReadAllText("BankOfAmericaCreditCardTransactionEmail.txt"));
+
+        transaction.Last4.Should().Be("4321");
+        transaction.Payee.Should().Be("PAYPAL TWITCHINTER");
+        transaction.Amount.Should().Be(123.45m);
+        transaction.Date.Should().BeSameDateAs(new System.DateTime(2021, 1, 31));
+    }
+
+    [Fact]
     public void InvalidParseReturnsNull(){
         var transaction = parser.ParseEmail("Bad match");
 
