7d6bdb3f3d247698017787a2371d721a8316811b
Aaron Smith <aaronasmith@gmail.com>
2021-08-09 11:44:15 -0500

    Try setting culture info specifically for parsing currency and date.

--- YNABTransactionEmailParser/Parsers/ChaseParser.cs
+++ YNABTransactionEmailParser/Parsers/ChaseParser.cs
@@ -36,12 +36,12 @@ namespace YNABTransactionEmailParser.Parsers
             var last4 = Regex.Match(account ?? "", @"(?<Account>\d{4})")?.Groups?["Account"]?.Value;
 
             string rawDate = GetValue(htmlContent, "Date");
-            DateTime.TryParseExact(rawDate ?? "", "MMM d, yyyy \"at\" h:mm tt \"ET\"", CultureInfo.CurrentCulture, DateTimeStyles.AssumeLocal, out var date);
+            DateTime.TryParseExact(rawDate ?? "", "MMM d, yyyy \"at\" h:mm tt \"ET\"", CultureInfo.CreateSpecificCulture("en-US"), DateTimeStyles.AssumeLocal, out var date);
             
             var payee = GetValue(htmlContent, "Merchant");
 
             var rawAmount = GetValue(htmlContent, "Amount");
-            decimal.TryParse(rawAmount, NumberStyles.AllowCurrencySymbol | NumberStyles.AllowDecimalPoint, CultureInfo.CurrentCulture, out var amount);
+            decimal.TryParse(rawAmount, NumberStyles.AllowCurrencySymbol | NumberStyles.AllowDecimalPoint, CultureInfo.CreateSpecificCulture("en-US"), out var amount);
 
             logger.LogInformation("RawAccount: {RawAccount}; RawDate: {RawDate}, RawAmount: {RawAmount}", account, rawDate, rawAmount);
             logger.LogInformation("Last4: {Last4}; Payee: {Payee}, Date: {Date}, Amount: {Amount}", last4, payee, date, amount);
